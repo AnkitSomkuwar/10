04c9588864f464b9bd4288a38e18dadb61076ba8
ankit <somkankit998@gmail.com>
2024-01-21 06:50:02 +0000

    solution

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const path = require('path')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const app = express()
+app.use(express.json())
+
+const dbPath = path.join(__dirname, 'covid19IndiaPortal.db')
+
+let db = null
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3000, () => {
+      console.log('Server Running at http://localhost:3000/')
+    })
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`)
+    process.exit(1)
+  }
+}
+
+initializeDBAndServer()
+
+const authentication = (request, response, next) => {
+  let jwtToken
+  const authHeader = request.headers['authorization']
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(' ')[1]
+  }
+  if (jwtToken === undefined) {
+    response.status(401)
+    response.send('Invalid JWT Token')
+  } else {
+    jwt.verify(jwtToken, 'MY_ANKIT', (error, payLoad) => {
+      if (error) {
+        response.status(401)
+        response.send('Invalid  JWT Token')
+      } else {
+        next()
+      }
+    })
+  }
+}
+
+app.post('/login/', authentication, async (request, response) => {
+  const {username, password} = request.body
+
+  const selectUserQuery = `
+  SELECT * 
+  FROm user 
+  WHERE
+  Username = "${username}"
+  `
+  const dbUser = await db.get(selectUserQuery)
+  if (dbUser === undefined) {
+    response.status(400)
+    response.send('Invalid user')
+  } else {
+    const isPasswordCorrect = await bcrypt.compare(password, dbUser.password)
+    if (isPasswordCorrect === true) {
+      const payLoad = {username: username}
+      const jwtToken = jwt.sign(payLoad, 'MY_ANKIT')
+      response.send({jwtToken})
+    } else {
+      response.status(400)
+      response.send('Invalid password')
+    }
+  }
+})
+
+module.exports = app
+
+//https://github.com/AnkitSomkuwar/10.git
